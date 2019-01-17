b11798ee2aea72ccfcfb19e46f93f03a16457b9c
Artem Krinitsyn <a.krinitsyn@qiwi.com>
2019-01-17 17:35:33 +0300

    Add popup demo

--- Node/examples/bill-payments-redirect-example/app.js
+++ Node/examples/bill-payments-redirect-example/app.js
@@ -1,18 +1,10 @@
-module.exports = ({ client, public_key, success_url = '' }) => {
+module.exports = ({public_key}) => {
     return (req, res) => {
         const amount = req.body.amount;
 
-        const bill_id = client.generateId();
-
-        const redirect = client.createPaymentForm({
-            public_key,
-            amount,
-            bill_id,
-            success_url
-        });
-
         const data = {
-            redirect
+            public_key,
+            amount
         };
 
         res.send(data);
